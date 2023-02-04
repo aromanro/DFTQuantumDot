ed1c9186d461aad12cf9201245899c793270eefc
Adrian Roman <aroman_ro@yahoo.com>
2023-02-04 10:52:04 +0200

    Moved the more important classes for optimum-finding methods in a separate header, I might extend those methods in the future.

---
+++ DFT/SimpleDescentBase.h
@@ -0,0 +1,134 @@
+#pragma once
+#include "DftSolver.h"
+
+namespace DFT {
+
+	namespace SolutionFinders {
+
+		namespace Helpers {
+
+			template<class Solver> class GradientDir
+			{
+			public:
+				static Eigen::MatrixXcd GetDirection(Solver& /*solver*/, const Eigen::MatrixXcd& gradient)
+				{
+					return -gradient;
+				}
+			};
+
+			template<class Solver> class GradDirection : public GradientDir<Solver>
+			{
+			public:
+				static double GetBeta(const Eigen::MatrixXcd& /*gradient*/, const Eigen::MatrixXcd& /*oldGradient*/, const Eigen::MatrixXcd& /*dir*/, const Eigen::MatrixXcd& /*oldDir*/)
+				{
+					return 0;
+				}
+			};
+
+		}
+
+		namespace BaseClasses {
+
+			template<class Solver> class SteepestDescent
+			{
+			public:
+				SteepestDescent(Solver& theSolver) : solver(theSolver) { }
+
+				Eigen::MatrixXcd Descend(const Eigen::MatrixXcd& W, unsigned int iter = 600, double alpha = 0.00003)
+				{
+					Eigen::MatrixXcd res = W;
+
+					for (unsigned int i = 0; i < iter; ++i)
+					{
+						res -= alpha * solver.getgrad(res);
+
+						//TRACE(L"E: %f, for step: %d\n", solver.getE(res), i);
+					}
+
+					/*
+					CString str;
+					str.Format(L"E: %f", solver.getE(res));
+
+					AfxMessageBox(str);
+					*/
+
+					return res;
+				}
+
+			protected:
+				Solver& solver;
+			};
+
+			template<class Solver, class Direction> class Minimization
+			{
+			public:
+				Minimization(Solver& theSolver) : solver(theSolver) {}
+
+
+				Eigen::MatrixXcd Descend(const Eigen::MatrixXcd& W, unsigned int iter = 600, double alphat = 0.00003)
+				{
+					double alpha = 0;
+					double beta = 0;
+
+					Eigen::MatrixXcd res = W;
+					Eigen::MatrixXcd gradient, dir, gradTrial, gradDif, trial, oldDir, oldGrad;
+
+					for (unsigned int i = 0; i < iter; ++i)
+					{
+						// save the old values for direction and gradient
+						oldDir = dir;
+						oldGrad = gradient;
+
+						// get the gradient and direction for current position
+						// for conjugate gradient the direction will be adjusted using the old direction with some beta depending on the method
+						gradient = solver.getgrad(res);
+						dir = Direction::GetDirection(solver, gradient); // Set search direction
+
+						// the cosine of the angle between the gradient at current point and the vector for the last step
+						//TRACE(L"Angle cos: %f\n", i ? Solver::Dot(gradient, oldDir) / sqrt(Solver::Dot(gradient, gradient) * Solver::Dot(oldDir, oldDir)) : 1);
+
+						if (i)
+						{
+							// beta is zero for 'pure' line minimization algorithms
+							// non zero for conjugate gradient methods
+							beta = Direction::GetBeta(gradient, oldGrad, dir, oldDir);
+							dir += beta * oldDir;
+						}
+
+						trial = res + alphat * dir; // look ahead
+						gradTrial = solver.getgrad(trial); // Evaluate gradient at trial step
+						gradDif = gradient - gradTrial;
+
+						double dotdif = Solver::Dot(gradDif, dir);
+						if (abs(dotdif) < 1E-20) dotdif = 1E-20 * (dotdif < 0 ? -1 : 1);
+
+						alpha = alphat * Solver::Dot(gradient, dir) / dotdif; // Compute estimate of best step				
+
+						res += alpha * dir;
+
+						//TRACE(L"E: %f, for step: %d\n", solver.getE(res), i);
+					}
+
+					/*
+					CString str;
+					str.Format(L"E: %f", solver.getE(res));
+					AfxMessageBox(str);
+					*/
+
+					return res;
+				}
+
+			protected:
+				Solver& solver;
+			};
+
+		}
+	
+	
+		template<class Solver> class LineMinimization : public BaseClasses::Minimization<Solver, Helpers::GradDirection<Solver>>
+		{
+		public:
+			LineMinimization(Solver& solver) : BaseClasses::Minimization<Solver, Helpers::GradDirection<Solver>>(solver) {}
+		};
+	}
+}
